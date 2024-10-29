da73eb80ceca3d75f0edd768870d80c626ee5e5f
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-10-29 13:05:58 -0500

    Solucion Circulo

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
 
-#Ingrese su nombre: Perico
-#Hola, Perico
+#Ingrese el radio: 5
+#Perimetro: 31.4
+#Área: 78.5
 
-name = input("Enter your name: ")
-print(f"""Hello, {name} """)
+import math
+
+# Función para calcular el perímetro y el área de un círculo
+def calcular_circulo(radio):
+    perimetro = 2 * math.pi * radio
+    area = math.pi * (radio ** 2)
+    return perimetro, area
+
+#Ingrese el radio
+radio = float(input("Ingrese el radio: "))
+perimetro, area = calcular_circulo(radio)
+
+# Mostrar los resultados
+print(f"Perímetro: {perimetro:.1f}")
+print(f"Área: {area:.1f}")
